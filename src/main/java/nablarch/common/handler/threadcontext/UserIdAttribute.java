a5331497b52aba23a55de5668848426bc81d8ae8
kumano_goro <kumano.goro@tis.co.jp>
2020-01-31 13:37:32 +0900

    ユーザIDの取得元を変更できるポイントを作成

--- src/main/java/nablarch/common/handler/threadcontext/UserIdAttribute.java
+++ src/main/java/nablarch/common/handler/threadcontext/UserIdAttribute.java
@@ -10,6 +10,7 @@ import nablarch.fw.ExecutionContext;
  * HTTPセッション上に格納されているログインユーザIDを
  * スレッドコンテキストに格納する。
  * </pre>
+ *
  * @author Iwauo Tajima <iwauo@tis.co.jp>
  */
 public class UserIdAttribute implements ThreadContextAttribute<Object> {
@@ -18,28 +19,32 @@ public class UserIdAttribute implements ThreadContextAttribute<Object> {
      * <pre>
      * デフォルトでは{@link #getKey()}の値を使用する。
      * </pre>
+     *
      * @param sessionKey HTTPセッション上のキー名
      */
     public void setSessionKey(String sessionKey) {
         this.sessionKey = sessionKey;
     }
+
     /** ログインユーザIDが格納されているHTTPセッション上のキー名 */
     private String sessionKey = null;
-    
+
     /**
      * 未ログイン時にスレッドコンテキストに設定されるIDを設定する。
      * <pre>
      * 明示的にこの値を設定しなかった場合、
      * 未ログイン時にスレッドコンテキスト上のユーザIDは設定されない。
      * </pre>
+     *
      * @param anonymousId 未ログイン時にスレッドコンテキストに設定されるID
      */
     public void setAnonymousId(String anonymousId) {
         this.anonymousId = anonymousId;
     }
+
     /** 未ログイン時にスレッドコンテキストに設定されるID */
     private String anonymousId = null;
-    
+
     /**
      * {@inheritDoc}
      * <pre>
@@ -54,7 +59,6 @@ public class UserIdAttribute implements ThreadContextAttribute<Object> {
      * {@inheritDoc}
      * <pre>
      * スレッドコンテキストに格納するユーザIDの値は以下のように決定される。
-     * 
      *   1. HTTPセッション上のキー{@link #sessionKey} の値を取得する。
      *      その値がnullでなければスレッドコンテキストに設定する。
      *   2. HTTPセッション上の値がnullであり、かつ {@link #anonymousId} が
@@ -64,12 +68,16 @@ public class UserIdAttribute implements ThreadContextAttribute<Object> {
      */
     public Object getValue(Object req, ExecutionContext ctx) {
         String skey = StringUtil.isNullOrEmpty(sessionKey)
-                    ? this.getKey()
-                    : sessionKey;
-        String userId = (String) ctx.getSessionScopedVar(skey);
+                ? this.getKey()
+                : sessionKey;
+        String userId = (String) getUserIdSession(ctx, skey);
         if (anonymousId != null && userId == null) {
             userId = anonymousId;
         }
         return userId;
+    }
+
+    protected Object getUserIdSession(ExecutionContext ctx, String skey) {
+        return ctx.getSessionScopedVar(skey);
     }
 }
